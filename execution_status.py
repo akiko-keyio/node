839d0b7b907351e7f5bcd54018fc73d76338bd1d
Aki <838013444@qq.com>
2025-06-14 16:28:15 +0800

    Add

---
+++ execution_status.py
@@ -0,0 +1,27 @@
+from node.node import Flow
+import time
+
+flow = Flow()
+
+
+@flow.node()
+def add(x: int, y: int) -> int:
+    time.sleep(2)
+    return x + y
+
+
+@flow.node()
+def square(x: int) -> int:
+    time.sleep(3)
+    return x * x
+
+
+@flow.node()
+def inc(x: int) -> int:
+    time.sleep(0.2)
+    return x + 1
+
+#
+if __name__ == "__main__":
+    node = square(add(square(2), square(2)))
+    print("Result:", node.get())
